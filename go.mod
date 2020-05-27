f127b31994097cf6369b0135eb22e7f981a9021e
crist_co <zeidoo@gmail.com>
2020-05-27 22:17:27 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zeidoo/go-sugar
+
+go 1.14
+
+require google.golang.org/grpc v1.29.1
---
+++ grpc/server.go
@@ -0,0 +1,23 @@
+package grpc
+
+import (
+	rpc "google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func StartGrpc(address string, registerServices func(*rpc.Server)) {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("listening on %s", lis.Addr())
+
+	s := rpc.NewServer()
+	registerServices(s)
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
